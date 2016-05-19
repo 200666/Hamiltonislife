769522e3e70776ec08a79f56eecff88c21c2d681
200666 <200666@hkis.edu.hk>
2016-05-19 08:27:23 +0800

    added the first jquerry function

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
 
+$("#play").click(function(){
+$("#realdealBody").css("background-image","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)")
+ });
 //Your jquery goes here
 
 
