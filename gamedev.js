6770de2db6e7561e49f757fe5b1eb7133e78e0b7
200666 <200666@hkis.edu.hk>
2016-05-19 08:35:38 +0800

    added new button on real deal mates

--- gamedev.js
+++ gamedev.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-$("#play").click(function(){
+$("#ready").click(function(){
 $("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
  });
 //Your jquery goes here
