a55170592bfeb93bdeeba35b2f7f78ec747253ef
200666 <200666@hkis.edu.hk>
2016-05-19 08:32:39 +0800

    changed jquery bits and added </a> tag

--- gamedev.js
+++ gamedev.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 $("#play").click(function(){
-$("#realdealBody").css("background-image","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)")
+$("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
  });
 //Your jquery goes here
 
