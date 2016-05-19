14011c0e5c2824a4d8bc6eebb1f04f43d96e2a76
200666 <200666@hkis.edu.hk>
2016-05-19 08:53:13 +0800

    tried adding java script function

--- gamedev.js
+++ gamedev.js
@@ -7,3 +7,10 @@ $("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/50743
 
 
 });
+function hamilton() {
+  var qone = prompt("you are alexandar hamilton. you enter the winters ball. you see many young gentalmen around you
+  	flirting with young ladies everywhere. suddenly, you see a woman walk directly towards you. 'hello' she says, looking you up and down, 'i am 
+  	angelica schuyler. whats yours?' a voice rings in your head, if you can get a schuyler sister your rich, son. is this your chance to
+  	escape poverty? suddenly, you, lost in your train of thought, realize that she asked you a question. but what if
+  	she turns you down do to your lack of money? do you lie and say you are loaded, or charm your way around the question?").toLowerCase();
+}
