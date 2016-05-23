7b3628dddb21d06d7481eb4c76e5446fd036a2f6
200666 <200666@hkis.edu.hk>
2016-05-23 08:41:26 +0800

    hide and showed butotns and made big div

--- gamedev.js
+++ gamedev.js
@@ -1,10 +1,13 @@
 $( document ).ready(function() {
-$("#q1").hide();
+$(".question").hide();
+$(".choice").hide();
 
 $("#ready").click(function(){
 $("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
 $("#ready").hide();
 $("#q1").show();
+$("#c1").show();
+$("#c2").show();
  });
 //Your jquery goes here
 
