bc4212d50d24a34c347482f4c568bf917f3b7bc6
200666 <200666@hkis.edu.hk>
2016-05-23 08:13:10 +0800

    changed button and tried to hide button

--- gamedev.js
+++ gamedev.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
 
-$("#play").click(function(){
+$("#ready").click(function(){
 $("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
+$("#ready").hide();
  });
 //Your jquery goes here
 
