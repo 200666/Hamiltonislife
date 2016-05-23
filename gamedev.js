81e5f32e3afd625e255133ade0099275d597df53
200666 <200666@hkis.edu.hk>
2016-05-23 08:16:49 +0800

    chose when to hide/show q1 div

--- gamedev.js
+++ gamedev.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
+$("#q1").hide();
 
 $("#ready").click(function(){
 $("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
 $("#ready").hide();
+$("#q1").show();
  });
 //Your jquery goes here
 
