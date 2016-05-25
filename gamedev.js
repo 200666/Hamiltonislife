f0efaa4fcbac39a354e5a04aeaba81319961dfa9
200666 <200666@hkis.edu.hk>
2016-05-25 09:41:45 +0800

    hid q3

--- gamedev.js
+++ gamedev.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 $(".question").hide();
 $(".choice").hide();
 $("#over1").hide();
+$("#q3").hide();
 
 $("#ready").click(function(){
 $("#realdealBody").css("backgroundImage","url(http://cdn-img.instyle.com/sites/default/files/styles/480xflex/public/1441312835/090315-hamilton-1.jpg?itok=xSJ9k-Sy)");
