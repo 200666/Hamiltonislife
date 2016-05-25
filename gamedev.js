fa81d67e7e9b22e721076d809075fb597986c1a7
200666 <200666@hkis.edu.hk>
2016-05-25 09:48:48 +0800

    hid game over screen in beginge

--- gamedev.js
+++ gamedev.js
@@ -1,17 +1,18 @@
 $( document ).ready(function() {
 $(".question").hide();
 $(".choice").hide();
-$("#over1").hide();
+$(".gameover").hide();
 $("#q3").hide();
 
 $("#ready").click(function(){
-$("#realdealBody").css("backgroundImage","url(http://cdn-img.instyle.com/sites/default/files/styles/480xflex/public/1441312835/090315-hamilton-1.jpg?itok=xSJ9k-Sy)");
+$("#realdealBody").css("backgroundImage","url()");
 $("#ready").hide();
 $("#q1").show();
 $("#c1").show();
 $("#c2").show();
 $("#over1").hide();
 $("#q3").hide();
+$("#q2").hide();
  });
 $("#c1").click(function(){
 $("#realdealBody").css("backgroundImage","url(https://www.google.com.hk/search?q=game+over+screen&safe=strict&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiKtriph_TMAhUCKJQKHSMHDX8Q_AUIBygB&biw=1440&bih=778#imgrc=YlgQBGwVkgWNTM%3A)");
