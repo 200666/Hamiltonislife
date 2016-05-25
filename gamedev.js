aa4ccb66aed7eea77786a87d1f28462657214834
200666 <200666@hkis.edu.hk>
2016-05-25 09:39:16 +0800

    added q3

--- gamedev.js
+++ gamedev.js
@@ -12,16 +12,24 @@ $("#c2").show();
 $("#over1").hide();
  });
 $("#c1").click(function(){
-$("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
+$("#realdealBody").css("backgroundImage","url(https://www.google.com.hk/search?q=game+over+screen&safe=strict&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiKtriph_TMAhUCKJQKHSMHDX8Q_AUIBygB&biw=1440&bih=778#imgrc=YlgQBGwVkgWNTM%3A)");
 $("#").hide();
 $("#q1").hide();
 $(".gameover").show();
  });
+$("#c3").click(function(){
+$("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
+$(".gameover").hide();
+$("#q1").hide();
+$("#q3").show();
+$("#q2").hide();
+
+
 $("#c2").click(function(){
 $("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
 $(".gameover").hide();
 $("#q1").hide();
-$("#q2").show();
+$("#q2").hide();
 
  });
 //Your jquery goes here
